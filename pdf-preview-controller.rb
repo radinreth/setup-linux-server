1cd45f7c4122615789df1fd34b01f6fc4abb6b96
radin reth <radin.reth@gmail.com>
2021-07-22 13:49:48 +0700

    Update pdf-preview-controller.rb

--- pdf-preview-controller.rb
+++ pdf-preview-controller.rb
@@ -18,3 +18,25 @@ puts "*" * 100
 
 @site.update(latest_generated_pdf_name: "#{pdf_name}.pdf")
 send_data pdf, type: 'application/pdf', disposition: 'inline'
+
+
+
+render  pdf: pdf_name,
+        template: template_path,
+        layout: 'pdf',
+        orientation: 'Portrait',
+        lowquality: false,
+        page_offset: 0,
+        save_to_file: Rails.root.join('pdfs', "#{pdf_name}.pdf"),
+        disable_smart_shrinking: false,
+        zoom: 1,
+        book: false,
+        print_media_type: false,
+        dpi: 300,
+        encoding: 'utf8',
+        page_size: 'A4',
+        default_protocol: 'https',
+        header: { right: '[page] of [topage]' },
+        content_type: 'application/pdf',
+        # Delay for chartjs to execute before render pdf
+        javascript_delay: ENV['JS_DELAY_IN_MILLISECONDS']
